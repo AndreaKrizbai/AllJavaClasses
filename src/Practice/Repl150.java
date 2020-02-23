f09d8456cdc071b48dd8fd662ed806a60fab377c
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-02-23 17:02:52 -0500

    4th commit

---
+++ src/Practice/Repl150.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class Repl150 {
+
+    public static void printHollowRect(){
+        for(int row= 1; row<=5; row++){
+            for (int column=1; column<=5; column++){
+                if(row==1 || row==5 || column==1 || column==5){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printHollowRect();
+    }
+}
